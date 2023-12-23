785b07bd715359e1f20f68c2ad910b6653b6489f
gxshub <guoxin2009@gmail.com>
2023-12-24 00:05:57 +1100

    add examples

--- src/mopmc-src/optimizers/FrankWolfe.h
+++ src/mopmc-src/optimizers/FrankWolfe.h
@@ -5,29 +5,32 @@
 #ifndef MOPMC_FRANKWOLFE_H
 #define MOPMC_FRANKWOLFE_H
 
-#include <vector>
-#include <set>
-#include <cassert>
-#include <algorithm>
-#include <Eigen/Dense>
-#include "BaseOptimizer.h"
-#include "PolytopeTypeEnum.h"
 #include "../convex-functions/BaseConvexFunction.h"
 #include "../convex-functions/TotalReLU.h"
+#include "BaseOptimizer.h"
 #include "LinOpt.h"
 #include "LineSearch.h"
+#include "PolytopeTypeEnum.h"
+#include <Eigen/Dense>
+#include <algorithm>
+#include <cassert>
+#include <set>
+#include <vector>
 
 namespace mopmc::optimization::optimizers {
 
     template<typename V>
-    using Vector =  Eigen::Matrix<V, Eigen::Dynamic, 1>;
+    using Vector = Eigen::Matrix<V, Eigen::Dynamic, 1>;
 
     enum FWOption {
-        LINOPT, AWAY_STEP, BLENDED, BLENDED_STEP_OPT
+        LINOPT,
+        AWAY_STEP,
+        BLENDED,
+        BLENDED_STEP_OPT
     };
 
     template<typename V>
-    class FrankWolfe : public BaseOptimizer<V>{
+    class FrankWolfe : public BaseOptimizer<V> {
     public:
         explicit FrankWolfe() = default;
 
@@ -35,26 +38,23 @@ namespace mopmc::optimization::optimizers {
 
         int minimize(Vector<V> &point, const std::vector<Vector<V>> &Vertices) override;
 
-        mopmc::optimization::optimizers::LinOpt<V> linOpt1;
-        mopmc::optimization::optimizers::LineSearcher<V> lineSearcher1;
+        mopmc::optimization::optimizers::LinOpt<V> linOpt;
+        mopmc::optimization::optimizers::LineSearcher<V> lineSearcher;
         FWOption fwOption{};
         Vector<V> alpha;
-        std::set<V> activeSet;
-        bool doLineSearch{true};
+        std::set<uint64_t> activeSet;
+        //bool doLineSearch{true};
 
     private:
         Vector<V> argmin(const std::vector<Vector<V>> &Vertices);
-
+        void updateWithSimplexGradientDescent(const std::vector<Vector<V>> &Vertices);
         void updateWithForwardOrAwayStep();
-
         void computeForwardStepIndexAndVector(const std::vector<Vector<V>> &Vertices);
-
         void computeAwayStepIndexAndVector(const std::vector<Vector<V>> &Vertices);
-
         void initialize(const std::vector<Vector<V>> &Vertices);
 
         const V tolerance{1.e-8}, toleranceCosine = std::cos(90.01 / 180. * M_PI);
-        const V gamma0{static_cast<V>(0.1)}, scale1{0.5}, scale2{0.5}, scale3{0.99};
+        const V scale1{0.5}, scale2{0.5};
         int64_t dimension{}, size{};
         Vector<V> xCurrent, xNew, xNewEx, dXCurrent;
         const uint64_t maxIter = 1e2;
@@ -62,8 +62,7 @@ namespace mopmc::optimization::optimizers {
         uint64_t fwdInd{}, awyInd{};
         Vector<V> fwdVec, awyVec;
         bool isFwd{};
-
     };
-}
+}// namespace mopmc::optimization::optimizers
 
-#endif //MOPMC_FRANKWOLFE_H
+#endif//MOPMC_FRANKWOLFE_H
